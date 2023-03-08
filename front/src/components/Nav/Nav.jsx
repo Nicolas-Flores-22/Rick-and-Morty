41ba157385501ea7848c2bf8b5ab371a64e02ffe
Nicolas <nicolas_flores22@outlook.com>
2023-03-07 22:16:11 -0500

    Modificando Componente Nav.jsx, le quite las className de cada etiqueta de Link

--- front/src/components/Nav/Nav.jsx
+++ front/src/components/Nav/Nav.jsx
@@ -7,20 +7,12 @@ function Nav({ onSearch }) {
     return (
         <div className={style.navSearch}>
             <div className={style.navSearchDivLink}>
-                <Link className={style.navSearchDivLinkLogout} to="/">
-                    <button>LOGOUT</button>
-                </Link>
-                <Link className={style.navSearchDivLinkAbout} to="/about">
-                    <button>ABOUT</button>
-                </Link>
-                <Link className={style.navSearchDivLinkHome} to="/home">
-                    <button>HOME</button>
-                </Link>
-                <Link className={style.navSearchDivLinkFavorites} to="/favorites">
-                    <button>FAVORITES</button>
-                </Link>
+                <Link to="/"><button> LOGOUT </button></Link>
+                <Link to="/about"><button> ABOUT </button></Link>
+                <Link to="/home"><button> HOME </button></Link>
+                <Link to="/favorites"><button> FAVORITES </button></Link>
             </div>
-            <div className={style.navAgregar}>
+            <div >
                 <SearchBar onSearch={onSearch} />
             </div>
         </div>
